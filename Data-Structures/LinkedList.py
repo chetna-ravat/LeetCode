3aff23a8d58e316fbbd85358c207c805278cc9de
Chetna Ravat <chetnaravat23@gmail.com>
2023-02-06 23:22:31 -0500

    Implementation of linked list- Insert, Delete, Reverse, Print

--- Data-Structures/LinkedList.py
+++ Data-Structures/LinkedList.py
@@ -1,5 +1,5 @@
 
-# The variables(attributes) defined under constructer are accessible to all the functions(methods) in the class. 
+# The variables defined under constructer are accessible to all the func(methods)in the class. 
 # we access them using self.
 
 
@@ -12,17 +12,39 @@ class Node():
 class Linkedlist():
     def __init__(self):
         self.head = None
-    
-    # value argument in below function is not accessed by self. since it is simply used in the func below and will not be accessible outside this func 
 
-    def append(self, value):
+
+    def insert(self, value, index):
+        '''
+        Inserting before first Node
+        Inserting after any given Node
+        '''
+        
+        if index < 0:
+            raise "invalid index parameter"
+
+        n1 = Node(value)
+        if self.head == None:
+            self.head = n1
+        elif index == 0 and self.head != None:
+            n1.next = self.head
+            self.head = n1
+        else:     
+            n = 1
+            curr = self.head
+            while curr.next != None and n < index:
+                curr = curr.next
+                n += 1
+            n1.next = curr.next
+            curr.next = n1
+    
+    def insertAtEnd(self, value):
         curr = self.head
         prev = None
 
         # Our breaking condition below is after the last Node i.e. at None
         while curr != None :
             prev = curr
-
             # below is similar to i = i+1
             curr = curr.next
         
@@ -32,6 +54,83 @@ class Linkedlist():
         else:
             prev.next = node1
 
+    def insertInSortedList(self,value):
+        '''
+        **This function would only work in a sorted list**
+        Cases considered: 
+        Linkedlist is empty
+        Duplicate values 
+        '''
+        
+        n1 = Node(value)
+        if self.head == None:
+            self.head = n1
+        else:
+            curr = self.head
+            prev = None
+            while curr != None and curr.data < value:
+                prev = curr
+                curr = curr.next
+            
+            if prev == None:
+                n1.next = self.head
+                self.head = n1
+            else:
+                n1.next = prev.next
+                prev.next = n1
+        
+    def delete(self,pos):
+
+        '''
+        This function deletes a given position/Node
+        '''
+
+        if self.head == None:
+            return
+        
+        curr = self.head
+        n = 1
+        prev = None
+        while curr != None and n < pos:
+            prev = curr
+            curr = curr.next
+            n+=1
+     
+        # below condition checks if the given position is out of range 
+        if curr == None:
+            return
+        # below condition is for deleting the first node 
+        elif prev == None:
+            self.head = curr.next
+        else:
+            prev.next = curr.next
+
+    def deleteAtEnd(self):
+        if self.head == None:
+            return
+        
+        prev = None
+        curr = self.head
+        while curr.next != None:
+            prev = curr
+            curr = curr.next
+        # below condition deltes the last remaining node
+        if prev == None:
+            self.head = None
+        else:
+            prev.next = None
+
+    def reverseLinkedList(self):
+        p = None
+        q = None
+        r = self.head
+        while r != None:
+            p = q
+            q = r
+            r = r.next
+            q.next = p
+        self.head = q
+        return self.head
 
     def print(self):
 
@@ -45,32 +144,15 @@ class Linkedlist():
         print("None")
 
 
-    def delete(self):
-        if self.head == None:
-            return
-        
-        prev = None
-        curr = self.head
-
-        # Our breaking condition below is on the last Node i.e. at last.next and not at None
-
-        while curr.next != None:
-            prev = curr
-            curr = curr.next
-    
-        if prev == None:
-            self.head = None
-        else:
-            prev.next = None
-
     def printRecurse(self):
         self.printrecursive(self.head)
+        print("None")
 
     def printrecursive(self, curr):
         if curr == None:
-            return
+            return 
         else:
-            print(curr.data)
+            print(curr.data, "->", end = " ")
             self.printrecursive(curr.next)
         
     # def printrecursive(self, curr= Node(-1)):                   
@@ -89,12 +171,14 @@ class Linkedlist():
 
 
 
-
 result = Linkedlist()
-result.append(7)
-result.append(10)
-result.append(18)
-result.append(12)
+result.insert(10,0)
+result.insert(15,3)
+result.insertAtEnd(23)
+result.insertInSortedList(50)
+result.printRecurse()
+x = result.reverseLinkedList()
+
 result.printRecurse()
 # result.printrecursive()
 
